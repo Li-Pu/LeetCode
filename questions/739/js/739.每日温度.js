999615c834787803a52698c4c613485690ae9d2e
Li Pu <825369365@qq.com>
2022-04-14 16:55:06 +0800

    feat(739): 每日温度
    [js] 2nd commit

--- questions/739/js/739.每日温度.js
+++ questions/739/js/739.每日温度.js
@@ -1,18 +1,18 @@
 /**
  * @param {number[]} temperatures
  * @return {number[]}
- * time: 188ms space: 59.3MB
+ * time: 172ms space: 62.9MB
  */
  var dailyTemperatures = function (temperatures) {
     const res = Array(temperatures.length).fill(0);
-    for (let i = 1; i < temperatures.length; i++){
-        let j = i - 1;
-        while (j >= 0 && temperatures[j] < temperatures[i]) {
-            if (res[j] === 0) {
-                res[j] = i - j;
-            }
-            j--;
+    const stck = [];
+    for (let i = 0; i < temperatures.length; i++){
+        const t = temperatures[i];
+        while (stck.length > 0 && temperatures[stck[stck.length - 1]] < t) {
+            const idx = stck.pop();
+            res[idx] = i - idx;
         }
+        stck.push(i);
     }
     return res;
 };
