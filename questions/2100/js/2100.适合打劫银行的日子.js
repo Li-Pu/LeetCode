9d533e4bd4e70fdb1e3bd2b502a74f83631e856c
Li Pu <825369365@qq.com>
2022-04-16 22:05:26 +0800

    feat(2100): 适合打劫银行的日子
    [js] 2nd commit

--- questions/2100/js/2100.适合打劫银行的日子.js
+++ questions/2100/js/2100.适合打劫银行的日子.js
@@ -2,29 +2,29 @@
  * @param {number[]} security
  * @param {number} time
  * @return {number[]}
- * time: 6088ms space: 66.5MB
+ * time: 136ms space: 67.6MB
  */
 var goodDaysToRobBank = function (security, time) {
-    let stackDescrease = [];
-    let stackIncrease = [];
+    const len = security.length;
+    const left = Array(len).fill(0);
+    const right = Array(len).fill(0);
     const res = [];
 
-    for (let i = 0; i < security.length; i++) {
-        if (stackDescrease.length > 0 && security[stackDescrease[stackDescrease.length - 1]] < security[i]) {
-            stackDescrease = [];
+    for (let i = 1; i < len; i++) {
+        if (security[i] <= security[i - 1]) {
+            left[i] = left[i - 1] + 1;
         }
-        stackDescrease.push(i);
+    }
 
-        if (security[i - 1] > security[i]) {
-            stackIncrease = [];
+    for (let i = len - 2; i >= 0; i--) {
+        if (security[i] <= security[i + 1]) {
+            right[i] = right[i + 1] + 1;
         }
+    }
 
-        if (stackDescrease.length > time) {
-            stackIncrease.push(i);
-
-        }
-        if (stackIncrease.length > 0 && i - stackIncrease[0] >= time) {
-            res.push(stackIncrease.shift())
+    for (let i = 0; i < len; i++) {
+        if (left[i] >= time && right[i] >= time) {
+            res.push(i);
         }
     }
 
