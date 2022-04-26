f979b74c988a6cc49772348eda94b14599f0a37b
Li Pu <825369365@qq.com>
2022-04-26 18:14:16 +0800

    feat(73): 矩阵置零
    [js] 2nd commit

--- questions/73/js/73.矩阵置零.js
+++ questions/73/js/73.矩阵置零.js
@@ -1,29 +1,55 @@
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
- * time: 80ms space: 44.4MB
+ * time: 72ms space: 43.9MB
  */
 var setZeroes = function (matrix) {
     const m = matrix.length;
     const n = matrix[0].length;
 
-    const row = Array(m).fill(false);
-    const column = Array(n).fill(false);
+    let firstRow = false, firstColumn = false;
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (matrix[i][j] === 0) {
-                row[i] = true;
-                column[j] = true;
-            }
+    for (let i = 0; i < n; i++) {
+        if (matrix[0][i] === 0) {
+            firstRow = true;
+            break;
         }
     }
 
     for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (row[i] || column[j]) {
+        if (matrix[i][0] === 0) {
+            firstColumn = true;
+            break;
+        }
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
                 matrix[i][j] = 0;
             }
         }
     }
+
+    if (firstRow) {
+        for (let i = 0; i < n; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+
+    if (firstColumn) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
 };
+
