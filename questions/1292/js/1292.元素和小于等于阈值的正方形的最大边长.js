a41e0da62e84bc7877cb80479f9ae491eb52241a
Li Pu <825369365@qq.com>
2022-04-30 17:52:04 +0800

    feat(1292): 元素和小于等于阈值的正方形的最大边长
    [js] 2nd commit

--- questions/1292/js/1292.元素和小于等于阈值的正方形的最大边长.js
+++ questions/1292/js/1292.元素和小于等于阈值的正方形的最大边长.js
@@ -2,28 +2,34 @@
  * @param {number[][]} mat
  * @param {number} threshold
  * @return {number}
- * time: 176ms space: 51.8MB
+ * time: 88ms space: 49.1MB
  */
 var maxSideLength = function (mat, threshold) {
     let m = mat.length, n = mat[0].length;
-    const rowMat = new Array(m).fill(0).map(() => new Array(n).fill(0));
-    const columnMat = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    const prefixSumMat = new Array(m).fill(0).map(() => new Array(n).fill(0));
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            rowMat[i][j] = (j > 0 ? rowMat[i][j - 1] : 0) + mat[i][j];
-            columnMat[i][j] = (i > 0 ? columnMat[i - 1][j] : 0) + mat[i][j];
+            prefixSumMat[i][j] =
+                (j > 0 ? prefixSumMat[i][j - 1] : 0) +
+                (i > 0 ? prefixSumMat[i - 1][j] : 0) -
+                (i > 0 && j > 0 ? prefixSumMat[i - 1][j - 1] : 0) + mat[i][j]
         }
     }
 
     let ret = 0;
-    const mtr = new Array(m).fill(0).map(() => new Array(n).fill(0));
     for (let i = 0; i < Math.min(m, n); i++) {
         for (let j = 0; j < m - i; j++) {
             for (let k = 0; k < n - i; k++) {
-                mtr[j][k] += rowMat[j + i][k + i] - (k > 0 ? rowMat[j + i][k - 1] : 0) + (columnMat[j + i][k + i] - (j > 0 ? columnMat[j - 1][k + i] : 0)) - mat[j + i][k + i]
-                if (mtr[j][k] <= threshold && i + 1 > ret) {
+                const sum =
+                    prefixSumMat[j + i][k + i] -
+                    (j > 0 ? prefixSumMat[j - 1][k + i] : 0) -
+                    (k > 0 ? prefixSumMat[j + i][k - 1] : 0) +
+                    (j > 0 && k > 0 ? prefixSumMat[j - 1][k - 1] : 0)
+                if (sum <= threshold && i + 1 > ret) {
                     ret = i + 1;
+                    j = m - i;
+                    break;
                 }
             }
         }
@@ -34,5 +40,6 @@ var maxSideLength = function (mat, threshold) {
     }
 
     return ret;
+
 };
 
