2965568ecd14c2b4028c45b0f18d73583b3fd322
Li Pu <825369365@qq.com>
2022-05-07 16:14:33 +0800

    feat(88): 合并两个有序数组
    [js] 2nd commit

--- questions/88/js/88.合并两个有序数组.js
+++ questions/88/js/88.合并两个有序数组.js
@@ -4,27 +4,12 @@
  * @param {number[]} nums2
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
- * time: 56ms space: 41MB
+ * time: 68ms space: 41MB
  */
 var merge = function (nums1, m, nums2, n) {
+    let p = m-- + n-- - 1;
 
-    let tm = nums1.length - 1;
-    for (let i = m - 1; i >= 0; i--, tm--) {
-        nums1[tm] = nums1[i];
+    while (n >= 0) {
+        nums1[p--] = m >= 0 && nums1[m] >= nums2[n] ? nums1[m--] : nums2[n--];
     }
-
-    tm++;
-    let tk = tn = 0;
-
-    while (tm < nums1.length || tn < n) {
-        if (tn >= n) {
-            break;
-        }
-        if (tm >= nums1.length) {
-            nums1[tk++] = nums2[tn++];
-            continue;
-        }
-        nums1[tk++] = nums1[tm] <= nums2[tn] ? nums1[tm++] : nums2[tn++];
-    }
-
 };
