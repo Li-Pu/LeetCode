ea55186af21303df4d09006af1681b86b1884d9a
Li Pu <825369365@qq.com>
2022-05-08 18:50:20 +0800

    feat(1): 两数之和
    [js] 2nd commit

--- questions/1/js/1.两数之和.js
+++ questions/1/js/1.两数之和.js
@@ -2,16 +2,23 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * time: 196ms space: 41.7MB
+ * time: 72ms space: 43.4MB
  */
 var twoSum = function (nums, target) {
     const len = nums.length;
 
-    for (let i = 0; i < len; i++) {
-        for (j = i + 1; j < len; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    const numAndIndexs = nums.map((value, index) => [value, index]).sort((a, b) => a[0] - b[0]);
+    numAndIndexs
+
+    let left = 0, right = len - 1;
+    while (left < right) {
+        const sum = numAndIndexs[left][0] + numAndIndexs[right][0];
+        if (sum === target) {
+            return [numAndIndexs[left][1], numAndIndexs[right][1]];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
 
