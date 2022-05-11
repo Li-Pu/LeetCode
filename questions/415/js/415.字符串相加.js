bae7493bf099c6147d4218512a2bd875e0e2f93a
Li Pu <825369365@qq.com>
2022-05-11 19:28:12 +0800

    feat(415): 字符串相加
    [js] 2nd commit

--- questions/415/js/415.字符串相加.js
+++ questions/415/js/415.字符串相加.js
@@ -2,33 +2,29 @@
  * @param {string} num1
  * @param {string} num2
  * @return {string}
- * time: 88ms space: 43.1MB
+ * time: 72ms space: 43MB
  */
 var addStrings = function (num1, num2) {
     let len1 = num1.length, len2 = num2.length;
     const len = Math.max(len1, len2) + 1;
-    const ret = new Array(len).fill(0);
+    const ret = [];
 
     let addUp = 0;
     for (let i = 1; i < len; i++) {
         if (len1 >= i) {
-            addUp += num1[len1 - i] - '0';
+            addUp += num1.charAt(len1 - i) - '0';
         }
         if (len2 >= i) {
-            addUp += num2[len2 - i] - '0';
+            addUp += num2.charAt(len2 - i) - '0';
         }
-        addUp
-        ret[len - i] = '' + addUp % 10;
+        ret.push('' + addUp % 10);
         addUp = Math.floor(addUp / 10);
     }
 
 
     if (addUp !== 0) {
-        ret[0] = addUp;
-    } else {
-        ret.shift();
+        ret.push('' + addUp);
     }
 
-    return ret.join('');
+    return ret.reverse().join('');
 };
-
