34def95677441e0aa2c47d5763b218250cede355
Li Pu <825369365@qq.com>
2022-05-11 20:16:15 +0800

    feat(200): 岛屿数量
    [js] 1st commit

--- questions/200/js/200.岛屿数量.js
+++ questions/200/js/200.岛屿数量.js
@@ -1,33 +1,41 @@
 /**
  * @param {character[][]} grid
  * @return {number}
- * time: 68ms space: 43.6MB
+ * time: 88ms space: 50.1MB
  */
 var numIslands = function (grid) {
     const m = grid.length, n = grid[0].length;
     let ret = 0;
 
-    const dfs = (i, j) => {
-        grid[i][j] = '0';
-        if (i > 0 && grid[i - 1][j] === '1') {
-            dfs(i - 1, j);
-        }
-        if (j > 0 && grid[i][j - 1] === '1') {
-            dfs(i, j - 1);
-        }
-        if (i + 1 < m && grid[i + 1][j] === '1') {
-            dfs(i + 1, j);
-        }
-        if (j + 1 < n && grid[i][j + 1] === '1') {
-            dfs(i, j + 1)
-        }
-    }
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === '1') {
                 ret++;
-                dfs(i, j);
+                let stack = [[i, j]];
+                grid[i][j] = '0';
+                while (stack.length > 0) {
+                    const newStack = [];
+                    for (let k = 0; k < stack.length; k++) {
+                        const [a, b] = stack[k];
+                        if (a > 0 && grid[a - 1][b] === '1') {
+                            grid[a - 1][b] = '0';
+                            newStack.push([a - 1, b]);
+                        }
+                        if (b > 0 && grid[a][b - 1] === '1') {
+                            grid[a][b - 1] = '0';
+                            newStack.push([a, b - 1]);
+                        }
+                        if (a + 1 < m && grid[a + 1][b] === '1') {
+                            grid[a + 1][b] = '0';
+                            newStack.push([a + 1, b]);
+                        }
+                        if (b + 1 < n && grid[a][b + 1] === '1') {
+                            grid[a][b + 1] = '0';
+                            newStack.push([a, b + 1])
+                        }
+                    }
+                    stack = newStack;
+                }
             }
         }
     }
