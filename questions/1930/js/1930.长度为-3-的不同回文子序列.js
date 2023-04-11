79af0a0f691d092fa11b6802f605be0b5bc3865a
李璞 <825369365@qq.com>
2023-04-11 12:49:17 +0000

    feat(1930): 长度为 3 的不同回文子序列
    [js] 2nd commit

--- questions/1930/js/1930.长度为-3-的不同回文子序列.js
+++ questions/1930/js/1930.长度为-3-的不同回文子序列.js
@@ -1,38 +1,33 @@
 /**
  * @param {string} s
  * @return {number}
- * time: 572ms space: 51MB
+ * time: 588ms space: 48.1MB
  */
 var countPalindromicSubsequence = function (s) {
-    if (s.length < 3) {
-        return 0;
-    }
-    let t = s.substring(0, 3);
-    for (let i = 3; i < s.length; i++) {
-        if (s.at(i) !== s.at(i - 1) || s.at(i) !== s.at(i - 2) || s.at(i) !== s.at(i - 3)) {
-            t += s.at(i);
-        }
-    }
-    const obj = {}, len = t.length;
+    const obj = {}, len = s.length;
     let count = 0;
     for (let i = 0; i < len; i++) {
-        const a = t.at(i);
+        const a = s.at(i);
         if (obj[a]) {
             continue;
         }
         const ai = {};
         for (let j = i + 1; j < len; j++) {
-            const b = t.at(j);
-            if (ai[b]) {
+            const b = s.at(j);
+            if (b in ai) {
                 continue;
             }
-            for (let k = j + 1; k < len; k++) {
-                const c = t.at(k);
+            let k = j + 1;
+            for (; k < len; k++) {
+                const c = s.at(k);
                 if (c === a) {
                     ai[b] = true;
                     count++;
                     break;
                 }
+            }
+            if (k === len) {
+                ai[b] = false;
             }
         }
         obj[a] = ai;
