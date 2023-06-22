6f7bc2d86926fe3f60ce5820588db971ce9155a7
Li Pu <825369365@qq.com>
2023-06-22 23:21:56 +0800

    feat(1): 两数之和
    [rust] 1st commit

---
+++ questions/1/rust/1.两数之和.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        for (i, num) in nums.into_iter().enumerate() {
+            if let Some(&x) = map.get(&(target - num)) {
+                return vec![x, i as i32];
+            }
+            map.insert(num, i as i32);
+        }
+        vec![]
+    }
+}
